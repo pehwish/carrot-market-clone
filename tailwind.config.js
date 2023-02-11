0df2a5a0bf8684dee8b1ae0ab801a82716cf711e
eunhye <pehwish@gmail.com>
2023-02-11 19:34:20 +0900

    init carrot-market-clone

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: 'media', // media 브라우저 모드 따라감 class => 클래스 추가하면 바뀜
+  plugins: [],
+};
